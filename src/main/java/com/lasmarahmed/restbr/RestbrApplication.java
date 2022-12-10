f1530c1b0fa5cdd9aa51e302300f17f5e1582da0
lassmar74 <ahmedlasmar.pro@gmail.com>
2022-12-10 11:41:41 +0100

    First Commit

---
+++ src/main/java/com/lasmarahmed/restbr/RestbrApplication.java
@@ -0,0 +1,13 @@
+package com.lasmarahmed.restbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lasmarahmed/restbr/RestbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasmarahmed.restbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
